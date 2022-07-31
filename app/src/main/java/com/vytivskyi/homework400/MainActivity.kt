689d434ff8fbc29ebcdf5d65aabe41e93fb4a9c2
banan <banana13251@gmail.com>
2022-07-31 13:01:46 +0300

    second codelab from homework 4.0.0

--- app/src/main/java/com/vytivskyi/homework400/MainActivity.kt
+++ app/src/main/java/com/vytivskyi/homework400/MainActivity.kt
@@ -6,20 +6,27 @@ import android.os.Bundle
 import com.vytivskyi.homework400.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var second_message: String
     override fun onCreate(savedInstanceState: Bundle?) {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val message2: String? = intent.getStringExtra("send2")
-        binding.textView.text = message2
+        second_message = intent.getStringExtra("send2")?: " "
+        binding.textView.text = savedInstanceState?.getString("message2")?: second_message
 
         binding.btSend.setOnClickListener{
             val i = Intent(this@MainActivity, SecondActivity::class.java)
             val message: String = binding.edSend.text.toString()
             i.putExtra("send1", message)
             startActivity(i)
+            finish()
         }
-
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("message2", second_message)
+    }
+
 }
--- app/src/main/java/com/vytivskyi/homework400/SecondActivity.kt
+++ app/src/main/java/com/vytivskyi/homework400/SecondActivity.kt
@@ -6,19 +6,29 @@ import android.os.Bundle
 import com.vytivskyi.homework400.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var first_message: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        val binding = ActivitySecondBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        val binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val message = intent.getStringExtra("send1")
-        binding.textView2.text = message
+        first_message = intent.getStringExtra("send1")?: " "
+        binding.textView2.text = savedInstanceState?.getString("message1")?: first_message
 
         binding.btSend2.setOnClickListener{
             val i = Intent(this@SecondActivity, MainActivity::class.java)
             val message2: String = binding.edSend2.text.toString()
             i.putExtra("send2",message2)
             startActivity(i)
+            finish()
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("message1", first_message)
+    }
+
+
 }
